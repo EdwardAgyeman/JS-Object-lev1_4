7f8d1c5c6c06e41917d75a714f5dc37865100cbb
Edward Agyeman <edw.agyeman@gmail.com>
2020-03-16 14:26:55 +0100

    done

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+var unsereHaustiere = [
+    {
+        tiertyp: "Katze",
+        names: [
+            "Gipsy",
+            "Nala",
+            "Dinky"
+        ]
+    },
+    {
+        tiertyp: "Hunde",
+        names: [
+            "Knöpfchen",
+            "Pinselchen",
+            "Droopy"
+        ]
+    },
+];
+
+console.log(unsereHaustiere[0].names[1])
+console.log(unsereHaustiere[1].names[2])
+console.log(unsereHaustiere[1].names)
